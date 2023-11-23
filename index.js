93063503c9858abf56d76d7462785073673c09dc
Evi1Jesus <d.gnezdilova2018@yandex.ru>
2023-11-23 15:01:44 +0500

    исправила блок с датой

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ app.use(express.static('./static'))
 
 const notes = [
     { name: 'Заметка 1', text: 'dfaesgrtdh', date: new Date() },
-    { name: 'Заметка 2', text: 'daesrh', date: 0 },
-    { name: 'Заметка 3', text: 'fasgrhdtyj', date: 0 },
-    { name: 'Заметка 4', text: 'grhetyu', date: 0 },
+    { name: 'Заметка 2', text: 'daesrh', date: new Date() },
+    { name: 'Заметка 3', text: 'fasgrhdtyj', date: new Date()},
+    { name: 'Заметка 4', text: 'grhetyu', date: new Date() },
 ]
 
 app.get('/api/notes', (request, response)=>{
     response.status(200)
     response.header('Content-Type', 'application/json')
     response.send(JSON.stringify(notes))
-}) // написали обработчик get метода к 
+}) // написали обработчик get метода
 
 app.post('/api/notes', (request, response)=>{
     request.body
     response.status(200)
     response.header('Content-Type', 'application/json')
-}) // написали обработчик get метода к 
+}) // написали обработчик post метода
 
 app.listen(80, (error)=>{
     if (!!error) {
