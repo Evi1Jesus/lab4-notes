d49d2bc7e1946c7dd6e1f975bd8ed264ffa302eb
Evi1Jesus <d.gnezdilova2018@yandex.ru>
2023-11-24 17:18:42 +0500

    - написали логику для todo и дописали логику для заметок - написали html разметку для todo

--- static/script.js
+++ static/script.js
@@ -1,66 +1,144 @@
+const createElement = (selector, children, options) => {
+    const [tag, ...classNames] = selector.split('.')
+
+    const elm = document.createElement(tag)
+    elm.classList.add(...classNames)
+
+    if (typeof children === 'string')
+        elm.innerText = children
+    else if (Array.isArray(children))
+        for (const child of children)
+            elm.appendChild(child)
+
+    if (typeof options === 'object' && options !== null)
+        for (const key of Object.keys(options))
+            if (/^on[A-Z]\w+$/.test(key))
+                elm.addEventListener(key.slice(2).toLowerCase(), options[key])
+            else if (key in elm)
+                elm[key] = options[key]
+
+    return elm
+}
+
+const refreshNotes = () => {
+    fetch('/api/note', { method: 'get' })
+        .then((data) => data.json())
+        .then((data) => refreshNoteList(data))
+}
+
 const refreshNoteList = (notes) => {
     const noteListElm = document.querySelector('.note-list')
     noteListElm.innerHTML=''
-    for (let note of notes){
-        let card = document.createElement('div')
-        card.classList.add('card')
-
-        let title = document.createElement('span')
-        title.classList.add('card-title')
-        title.innerText = note.name
-        card.appendChild(title)
-
-        let body = document.createElement('span')
-        body.classList.add('card-body')
-        body.innerText = note.text
-        card.appendChild(body)
-
-        const bottomBlock = document.createElement('div')
-        bottomBlock.classList.add('bottom-block')
-
-        const changeDate = document.createElement('div')
-        changeDate.classList.add('date')
-
-        const dateLabel = document.createElement('span')
-        dateLabel.innerHTML = 'Дата изменения заметки:'
-        changeDate.appendChild(dateLabel)
-        const dateValue = document.createElement('span')
-        dateValue.innerHTML = new Date(note.date).toISOString()
-        changeDate.appendChild(dateValue)
-        bottomBlock.appendChild(changeDate)
-
-        const buttons = document.createElement('div')
-        buttons.classList.add('buttons')
-
-        let buttonChange = document.createElement('button')
-        buttonChange.classList.add('change')
-        buttonChange.innerText = 'Редактировать'
-        buttonChange.addEventListener('click', () => {
-            const index = notes.findIndex((value)=> value == note)
-            
-        })
-        buttons.appendChild(buttonChange)
-
-        let buttonDel = document.createElement('button')
-        buttonDel.classList.add('del')
-        buttonDel.innerText = 'Удалить заметку'
-        buttonDel.addEventListener('click', () => {
-            const index = notes.findIndex((value)=> value == note)
-            if(index<0){
-                return
-            }
-            notes.splice(index, 1)
-            refreshNoteList()
-        })
-        buttons.appendChild(buttonDel)
-
-        bottomBlock.appendChild(buttons)
-        card.appendChild(bottomBlock)
+    for (let i = 0; i < notes.length; i++) {
+        const note = notes[i]
+        const card = createElement('div.card', [
+            createElement('span.card-title', note.name),
+            createElement('span.card-body', note.text),
+            createElement('div.bottom-block', [
+                createElement('div.date', [
+                    createElement('span', 'Дата изменения заметки:'),
+                    createElement('span', new Date(note.date).toISOString()),
+                ]),
+                createElement('div.buttons', [
+                    createElement('button.change', 'Редактировать', { onClick: () => {
+                        const idElem = document.querySelector('.write-note > [name="id"]') // смотрим айди новой или старой заметки
+                        idElem.value = i
+                        const noteNameElem = document.querySelector('.write-note > [name="label"]')
+                        const noteTextElem = document.querySelector('.write-note > [name="description"]')
+                        noteNameElem.value = note.name
+                        noteTextElem.value = note.text
+                    } }),
+                    createElement('button.del', 'Удалить заметку', { onClick: () => {
+                        fetch('/api/note', {
+                            method: 'delete',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify({ noteId: i }),
+                        }).then(refreshNotes)
+                    } }),
+                ]),
+            ]),
+        ])
 
         noteListElm.appendChild(card)
     }
 }
 
-fetch('/api/notes', { method: 'get' })
-    .then((data) => data.json())
-    .then((data) => refreshNoteList(data))
+const refreshToDos = () => {
+    fetch('/api/todo', { method: 'get' })
+        .then((data) => data.json())
+        .then((data) => refreshToDoList(data))
+}
+
+const refreshToDoList = (toDos) => {
+    const toDoListElm = document.querySelector('.todo-list')
+    toDoListElm.innerHTML=''
+    for (let i = 0; i < toDos.length; i++) {
+        const toDo = toDos[i]
+        const card = createElement('div.card', [
+            createElement('span.card-title', toDo.name),
+            createElement('span.card-body', toDo.text),
+            createElement('div.bottom-block', [
+                createElement('div.date', [
+                    createElement('span', 'Выполнена:'),
+                    createElement('span', toDo.done ? 'Да' : 'Нет'),
+                ]),
+                createElement('div.buttons', [
+                    createElement('button.change', 'Редактировать', { onClick: () => {
+                        const idElem = document.querySelector('.write-todo > [name="id"]') // смотрим айди новой или старой заметки
+                        idElem.value = i
+                        const toDoNameElem = document.querySelector('.write-todo > [name="label"]')
+                        const toDoTextElem = document.querySelector('.write-todo > [name="description"]')
+                        toDoNameElem.value = toDo.name
+                        toDoTextElem.value = toDo.text
+                    } }),
+                    createElement('button.del', 'Удалить дело', { onClick: () => {
+                        fetch('/api/todo', {
+                            method: 'delete',
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify({ noteId: i }),
+                        }).then(refreshToDos)
+                    } }),
+                ]),
+            ]),
+        ])
+
+        toDoListElm.appendChild(card)
+    }
+}
+
+const noteFormElem = document.querySelector('.write-note')
+const toDoFormElem = document.querySelector('.write-todo')
+
+noteFormElem.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    const formData = new FormData(noteFormElem)
+    const noteId = parseInt(formData.get('id'))
+    fetch('/api/note', {
+        method: Number.isNaN(noteId) ? 'post' : 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            noteId: Number.isNaN(noteId) ? undefined : noteId,
+            name: formData.get('label'),
+            text: formData.get('description'),
+        })
+    }).then(refreshNotes)
+})
+
+toDoFormElem.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    const formData = new FormData(toDoFormElem)
+    const toDoId = parseInt(formData.get('id'))
+    fetch('/api/todo', {
+        method: Number.isNaN(toDoId) ? 'post' : 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            toDoId: Number.isNaN(toDoId) ? undefined : toDoId,
+            name: formData.get('label'),
+            text: formData.get('description'),
+            done: Boolean(formData.get('done')),
+        })
+    }).then(refreshToDos)
+})
+
+refreshNotes()
+refreshToDos()
